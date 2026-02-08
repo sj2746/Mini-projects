798d4be5a0011bb40a3d0a82a3facb8a0724276b
sajan <bhatsajan2005@gmail.com>
2026-02-08 16:32:39 +0530

    updated WeatherApp

--- WeatherApp(API)/script.js
+++ WeatherApp(API)/script.js
@@ -1,15 +1,41 @@
-function getTemp() {
-  let city = document.getElementById("city").value;
-  let result = document.getElementById("result");
+const API_KEY = "38240e6684c248eea58105218260702"
+const input = document.getElementById("input")
 
-  let url = `https://api.weatherapi.com/v1/current.json?key=38240e6684c248eea58105218260702&q=${city}&aqi=no`;
+//loc and country
+const locationCity=document.getElementById("loc_inp")
+const country=document.getElementById("country_name")
+//temp
+const temperature=document.getElementById("temp")
+//weather
+const weather=document.getElementById("weather_type")
+//humidity
+const humidity=document.getElementById("humidity")
+//time
+const time=document.getElementById("time")
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      result.innerHTML = `Temperature in ${data.location.name} is ${data.current.temp_c}°C`;
+const weather_card=document.getElementById("weather_card")
+
+
+
+weather_card.style.display="none"
+function searchCity() {
+    const city = input.value;
+    const URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=yes`
+    fetch(URL)
+    .then((response)=>response.json())
+    .then((data)=>{
+        const imgURL=`http:${data.current.condition.icon}`
+        weather_card.style.backgroundImage=fetch(imgURL)
+        weather_card.style.display="block"
+        locationCity.innerHTML=data.location.name
+        country.innerHTML=data.location.country
+        temperature.innerHTML=`${data.current.temp_c} <sup>0</sup>c`
+        weather.innerHTML=data.current.condition.text
+        humidity.innerHTML=data.current.humidity
+        time.innerHTML=data.location.localtime
+
     })
-    .catch(() => {
-      result.innerHTML = "City not found!";
+    .catch((error)=>{
+        console.log(error)
     });
 }
